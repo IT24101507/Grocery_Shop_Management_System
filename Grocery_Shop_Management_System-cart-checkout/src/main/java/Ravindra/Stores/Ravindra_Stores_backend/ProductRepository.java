4ea9265c82a5274078d2250f6ce857b7b0912c27
IT24101507 <it24101507@my.sliit.lk>
2025-10-12 23:21:07 +0530

    Update ProductRepository.java

--- Grocery_Shop_Management_System-cart-checkout/src/main/java/Ravindra/Stores/Ravindra_Stores_backend/ProductRepository.java
+++ Grocery_Shop_Management_System-cart-checkout/src/main/java/Ravindra/Stores/Ravindra_Stores_backend/ProductRepository.java
@@ -1,10 +1,13 @@
 package Ravindra.Stores.Ravindra_Stores_backend;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
     // Find products by category
     List<Product> findByCategory(String category);
@@ -13,13 +16,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByNameContainingIgnoreCase(String name);
 
     // Find products cheaper than or equal to a given price
-    List<Product> findByPriceLessThanEqual(double price);
+    List<Product> findBySalePriceLessThanEqual(BigDecimal price);
 
     // Find products more expensive than or equal to a given price
-    List<Product> findByPriceGreaterThanEqual(double price);
+    List<Product> findBySalePriceGreaterThanEqual(BigDecimal price);
 
     // Find products in a price range
-    List<Product> findByPriceBetween(double minPrice, double maxPrice);
+    List<Product> findBySalePriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
 
     // Find all products that have quantity greater than the given value
     List<Product> findByDisplayQuantityGreaterThan(int mindisplayQuantity);
