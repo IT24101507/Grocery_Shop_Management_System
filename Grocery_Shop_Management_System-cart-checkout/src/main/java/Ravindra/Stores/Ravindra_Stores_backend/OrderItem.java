81f188a024f16e5a1e7c6baa3581a2a9b356fed1
IT24101507 <it24101507@my.sliit.lk>
2025-10-12 23:06:06 +0530

    order

---
+++ Grocery_Shop_Management_System-cart-checkout/src/main/java/Ravindra/Stores/Ravindra_Stores_backend/OrderItem.java
@@ -0,0 +1,38 @@
+package Ravindra.Stores.Ravindra_Stores_backend;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+    private int quantity;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    @JsonBackReference
+    private Order order;
+}
