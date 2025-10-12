2b338fa9ffed0935c420d640f1ae41c0ba753e48
IT24101507 <it24101507@my.sliit.lk>
2025-10-12 22:11:06 +0530

    Update PaaswordChangeToken.java

---
+++ src/main/java/Ravindra/Stores/Ravindra_Stores_backend/PaaswordChangeToken.java
@@ -0,0 +1,64 @@
+package Ravindra.Stores.Ravindra_Stores_backend;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class PasswordChangeToken {
+    private static final int EXPIRATION = 60 * 24;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id", foreignKey = @ForeignKey(name = "FK_USER_PASS_TOKEN"))
+    private User user;
+
+    private Date expiryDate;
+
+    public PasswordChangeToken() {
+        super();
+    }
+
+    public PasswordChangeToken(final String token, final User user) {
+        super();
+        this.token = token;
+        this.user = user;
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+    }
+
+    private Date calculateExpiryDate(int expiryTimeInMinutes) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+}
