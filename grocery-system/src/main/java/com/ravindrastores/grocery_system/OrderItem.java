2ceb1eeab45e218342e8e6ffa24c19f0156a9738
DahamKu <it24101516@my.sliit.lk>
2025-10-27 01:56:15 +0530

    Update OrderItem.java

--- grocery-system/src/main/java/com/ravindrastores/grocery_system/OrderItem.java
+++ grocery-system/src/main/java/com/ravindrastores/grocery_system/OrderItem.java
@@ -1,12 +1,20 @@
-package com.ravindrastores.grocery_system;
+package Ravindra.Stores.Ravindra_Stores_backend;
+
+import java.math.BigDecimal;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "order_items")
@@ -20,8 +28,9 @@ public class OrderItem {
     private Long id;
 
     private Long productId;
+    private String productName;
     private int quantity;
-    private double price;
+    private BigDecimal price;
 
     @ManyToOne
     @JoinColumn(name = "order_id")
